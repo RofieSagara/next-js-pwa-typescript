714641f1d87d0421c4166453dee8df51190b2ee1
RofieSagara <sagararofie@gmail.com>
2022-01-31 11:43:11 +0700

    first commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const hello = (req, res) => {
+  res.status(200).json({ name: 'Next js + PWA + Typescript' })
+}
+
+export default hello
